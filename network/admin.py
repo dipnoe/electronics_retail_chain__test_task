56e7147a1671656042749e37d84c358a9f4e37da
Vagner Elena <dipnoe@gmail.com>
2023-10-31 13:32:14 +0700

    feat(admin): register admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import NetworkElement, Contact, Product
+
+
+@admin.register(NetworkElement)
+class NetworkElementAdmin(admin.ModelAdmin):
+    list_display = ['name', 'contacts', 'product', 'debt', 'created_at', 'level']
+    list_display_links = ['provider']
+    list_filter = ['provider__contacts__city']
+    actions = ["clear_debt"]
+
+    @admin.action(description="clears the debt to the supplier for selected objects")
+    def clear_debt(self, request, queryset):
+        queryset.update(debt=0)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['email', 'country', 'city', 'street', 'building_num']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'model', 'launch_date']
