c23d1fb45c6af48f751db22be46a6dbf585fb282
Vagner Elena <dipnoe@gmail.com>
2023-11-05 15:50:03 +0700

    fix(model): add documentation

--- users/models.py
+++ users/models.py
@@ -4,6 +4,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    """
+    Custom User Model for Authentication and User Management.
+
+    Fields:
+    - email: An email field used for user authentication and must be unique.
+    - first_name: A character field for the user's first name (maximum length: 50 characters).
+    - last_name: A character field for the user's last name (maximum length: 50 characters).
+    - role: A character field that represents the user's role. It uses the 'UserRole' choices.
+    - is_active: A boolean field indicating whether the user is active.
+
+    Choices:
+    - UserRole: A class that defines user roles. Currently, it includes a single role 'employee'.
+    """
     class UserRole(models.TextChoices):
         EMPLOYEE = 'EMP', _('employee')
 
